487deb1f5d80d2b0501a4524df00256cbf256916
soufiane <soufiane.compte.pro@outlook.fr>
2021-03-30 15:50:26 +0200

    v3

--- exo.py
+++ exo.py
@@ -1,15 +1,19 @@
 somme = 100
-continuer=False
 
 while somme > 0 :
-    choix = int(input("Choisissez un nombre entre 0 et 49 : "))
-
-    mise = int(input("Entrez votre mise : "))
-    if mise>somme:
-        print("Vous n'avez pas assez d'argent (",somme,"euros).")
-        continue
-    somme -= mise
     gagnant = 40 #randrange(50)
+
+    choix = int(input("\nChoisissez un nombre entre 0 et 49 : "))
+    if choix<0 or choix>49:
+        print("Restez dans l'intervalle [0-49].")
+
+    
+    mise = int(input("Entrez votre mise : "))
+    while mise>somme:
+        print("Vous n'avez pas assez d'argent (",somme,"euros).")
+        mise = int(input("Entrez votre mise : "))
+        
+    somme -= mise
 
     if choix == gagnant:
         mise = mise*3
@@ -17,11 +21,14 @@ while somme > 0 :
     elif (choix%2 == 0 and gagnant%2 == 0) or (choix%2 == 1 and gagnant%2 == 1):
         mise = mise*1.5
         somme +=mise
-        
-    print("\nVous avez maintenant ",somme, "euros.")
-    
+
+    if somme > 0:
+        print("\nVous avez maintenant ",somme, "euros.")
+    else:
+        print("Perdu, vous n'avez plus d'argent.")
+        break
+
     quit = input("Continuer(C), quitter(Q)  : ")
     quit = quit.lower()
     if quit == "q":
         break
-    
