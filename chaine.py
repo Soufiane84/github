4c0322247bc213e8ba248e6941e69723352e68ba
soufiane <soufiane.compte.pro@outlook.fr>
2021-03-31 11:26:09 +0200

    v1

---
+++ chaine.py
@@ -0,0 +1,61 @@
+# -*- coding : UTF-8 -*-
+
+""" 
+liste1 = ["lapin", "chat", "chien", "chiot", "dragon", "ornithorynque"]
+
+for elem in liste1:
+    print(elem)
+
+liste1.reverse()
+for elem in liste1:
+    print(elem)
+
+liste1.sort()
+for elem in liste1:
+    print(elem)
+
+liste1.append("troll")
+
+liste1.remove("troll")
+for elem in liste1:
+    print(elem) 
+
+ """
+
+
+
+
+ 
+import random
+
+"""
+tableau_jeu=[]
+for i in range (0,10) :
+    tableau_jeu.append(random.randint (1,10))
+print(tableau_jeu)
+
+nb = int(input("Entrez le chiffre : "))
+for i in range(0,len(tableau_jeu)):
+    if nb == tableau_jeu[i]:
+        print("Gagné")
+        break
+    if i == len(tableau_jeu)-1:
+        print("Perdu")
+"""
+
+
+tableau_jeu=[]
+for i in range (0,10) :
+    tableau_jeu.append(random.randint (1,10))
+tableau_jeu.sort()
+print(tableau_jeu)
+
+nb = int(input("Entrez le chiffre : "))
+for i in range(0,len(tableau_jeu)):
+    if nb == tableau_jeu[i]:
+        print("Gagné")
+        break
+    if i == len(tableau_jeu)-1 or tableau_jeu[i]>nb:
+        print("Perdu")
+        break
+
