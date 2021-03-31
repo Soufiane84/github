d0461e61f2837f75d71586688b37a9def5064236
soufiane <soufiane.compte.pro@outlook.fr>
2021-03-31 16:23:06 +0200

    v2

--- fich.py
+++ fich.py
@@ -15,7 +15,7 @@ print(position)
 def replace_multiple(texte, replace):
     for i, j in replace.items():
         texte = texte.replace(i, j)
-    print(texte)
+    return texte
 
 replace = {"1": "i", "3": "e"} 
 texte =  "La v13 3st un myst3r3 qu'1l faut v1vr3, et non un probl3m3 a r3soudr3. Gandh1"
@@ -23,3 +23,25 @@ texte =  "La v13 3st un myst3r3 qu'1l faut v1vr3, et non un probl3m3 a r3soudr3.
 res = replace_multiple(texte, replace)
 print(res)
 """
+
+
+""" 
+def compteur(lettre, chaine):
+    cpt=0
+    for letter in chaine:
+        if letter == lettre:
+            cpt+=1
+    return cpt
+
+res= compteur('a', "hgfhgf")
+print(res)
+"""
+
+
+def inverse(mot):
+    inv= mot[::-1]
+    return inv
+
+res = inverse("abc")
+print(res)
+
